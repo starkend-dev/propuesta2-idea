780d93974c466de29b20b0029574725f4e72e22c
Hector Pichardo <hectorpichardoe@outlook.com>
2019-07-30 17:34:44 -0500

    cambio mapa

--- js/config.js
+++ js/config.js
@@ -23,7 +23,9 @@ var config_videoType = 'video/mp4';
 var config_videoOpacity = 1;
 
 /* Google map parameters */
-var config_mapLat = 51.509036; // latitude
-var config_mapLng = -0.123280; // longitude
+// var config_mapLat = 51.509036; // latitude
+// var config_mapLng = -0.123280; // longitude
+var config_mapLat = 19.5219202; // latitude
+var config_mapLng = -99.21574049999998; // longitude
 var config_mapZoom = 16; // map zoom
 var config_mapTitle = 'My company' // marker title
